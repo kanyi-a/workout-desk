2527851e4087362ceafbe33233595c33a711c18e
kanyi-a <angela.kanyi@student.moringaschool.com>
2022-10-09 23:17:50 +0300

    milestone:adding the login page

---
+++ client/src/components/Login.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Login() {
+  return (
+    <div className='login-form'>
+      <Wrapper>
+            <Logo>Desk-Fit</Logo>
+            <h2 className='tagline'> Our handpicked collection of stretch exercises and videos will help ease your stress and the strain on your body</h2>
+
+      </Wrapper>
+
+
+
+    </div>
+  )
+}
+
+export default Login
