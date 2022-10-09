4d72cd0acc92a0af7db1fc21b25f13971f674c29
kanyi-a <angela.kanyi@student.moringaschool.com>
2022-10-10 00:34:53 +0300

    milestone: addinng the handle submit function and mapping the errors

---
+++ client/src/components/SignUpForm.js
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+import Button from "./styles/Button";
+import Label from "./styles/Label";
+import FormField from "./styles/FormField";
+import Error from "./styles/Error";
+import Input from "./styles/Input";
+
+function SignUpForm({ onLogin }) {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    setErrors([]);
+    setIsLoading(true);
+    fetch("/signup", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username,
+        password
+      }),
+    }).then((r) => {
+      setIsLoading(false);
+      if (r.ok) {
+        r.json().then((user) => onLogin(user));
+      } else {
+        r.json().then((err) => setErrors(err.errors));
+      }
+    });
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <FormField>
+        <Label htmlFor="username">Username</Label>
+        <Input
+          type="text"
+          id="username"
+          autoComplete="off"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </FormField>
+      <FormField>
+        <Label htmlFor="password">Password</Label>
+        <Input
+          type="password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          autoComplete="current-password"
+        />
+      </FormField>
+      <FormField>
+        <Button type="submit">{isLoading ? "Loading..." : "Sign Up"}</Button>
+      </FormField>
+      <FormField>
+        {errors.map((err) => (
+          <Error key={err}>{err}</Error>
+        ))}
+      </FormField>
+    </form>
+  );
+}
+
+export default SignUpForm;
