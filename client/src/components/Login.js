5a28d9cbb7715470f105c7f11da2f461612f3d44
kanyi-a <angela.kanyi@student.moringaschool.com>
2022-10-10 00:39:44 +0300

    milestone:importing the styled component files from the styles folder

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,18 +1,63 @@
-import React from 'react'
+import { useState } from "react";
+import styled from "styled-components";
+import LoginForm from "./LoginForm";
+import SignUpForm from "./SignUpForm";
+import Button from "./styles/Button";
 
-function Login() {
+
+function Login({ onLogin }) {
+  const [showLogin, setShowLogin] = useState(true);
+  
   return (
-    <div className='login-form'>
-      <Wrapper>
-            <Logo>Desk-Fit</Logo>
-            <h2 className='tagline'> Our handpicked collection of stretch exercises and videos will help ease your stress and the strain on your body</h2>
-
-      </Wrapper>
-
-
-
+    <div className="login-form">
+    <Wrapper>
+      <Logo>Desk-Fit</Logo>
+      <h2 className="tagline"> <strong> Welcome!! </strong> <br/> Our handpicked collection of stretching  exercises and poses will help ease the strain on your body and help you focus more.</h2>
+      {showLogin ? (
+        <>
+          <LoginForm onLogin={onLogin} />
+          <Divider />
+          <p className="accountquestion">
+            Don't have an account?</p> &nbsp;
+            <Button onClick={() => setShowLogin(false)}>
+              Sign Up
+            </Button> 
+        </>
+      ) : (
+        <>
+          <SignUpForm onLogin={onLogin} />
+          <Divider />
+          <p className="accountquestion">
+            Already have an account?</p> &nbsp;
+            <Button onClick={() => setShowLogin(true)}>
+              Log In
+            </Button>
+        </>
+      )}
+    </Wrapper>
     </div>
-  )
+  );
 }
 
-export default Login
+const Logo = styled.h1`
+  font-family: "Cookie", cursive;
+  color: black;
+  text-shadow: 1px 1px 10px white;
+  font-size: 100px;
+  margin: 8px 0 16px;
+  text-align: center;
+`;
+
+const Wrapper = styled.section`
+  max-width: 500px;
+  margin: 40px auto;
+  padding: 16px;
+`;
+
+const Divider = styled.hr`
+  border: none;
+  border-bottom: 1px solid #ccc;
+  margin: 16px 0;
+`;
+
+export default Login;
