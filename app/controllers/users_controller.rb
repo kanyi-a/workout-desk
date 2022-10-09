1e99bf3d23dc80e3e3eb8c7f56397c9412f5f84b
kanyi-a <angela.kanyi@student.moringaschool.com>
2022-10-10 01:03:44 +0300

    milestone:added an error inavalid method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+      rescue_from ActiveRecord::RecordInvalid, with: :unprocessable_entity_error
       def index
             users = User.all
             render json: users, status: :ok
@@ -18,5 +19,9 @@ class UsersController < ApplicationController
       def user_params
             params.permit(:username, :password)
       end
+      def unprocessable_entity_error(invalid)
+            render json: {errors: invalid.record.errors.full_messages}, status: :unprocessable_entity
+      end
+       
     
 end
