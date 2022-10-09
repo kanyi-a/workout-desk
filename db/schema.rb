0ffba23085cc317aef6d2d79f67551b98037245f
kanyi-a <angela.kanyi@student.moringaschool.com>
2022-10-09 21:45:37 +0300

    milestone:migrated the 4 tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,43 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2022_10_09_182716) do
+
+  create_table "add_admin_id_to_stretch_poses", force: :cascade do |t|
+    t.integer "admin_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "admins", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.integer "stretch_pose_id"
+  end
+
+  create_table "stretch_poses", force: :cascade do |t|
+    t.string "title"
+    t.string "image"
+    t.string "category"
+    t.string "description"
+    t.boolean "is_favoured"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
