29ad61151eb6cf9893d7529bf92b3548753123e8
kanyi-a <angela.kanyi@student.moringaschool.com>
2022-10-10 00:36:25 +0300

    milestone: adding the handle submit function and importing files from the style folder

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,70 +1,68 @@
-import React, { useState }from 'react'
-import { Button, Error, Input, FormField, Label } from "./styles";
+import React, { useState } from 'react';
+//import { Button, Error, Input, FormField, Label } from "./styles";
+import Button from "./styles/Button";
+import Label from "./styles/Label";
+import FormField from "./styles/FormField";
+import Error from "./styles/Error";
+import Input from "./styles/Input";
+export default function LoginForm({ onLogin }) {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
-export default function LoginForm () {
-      const [username, setUsername] = useState("");
-      const [password, setPassword] = useState("");
-      const [errors, setErrors] = useState([]);
-      const [isLoading, setIsLoading] = useState(false);  
+  function handleSubmit(e) {
+    e.preventDefault();
+    setIsLoading(true);
+    fetch("/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ username, password }),
+    })
+      .then((r) => {
+        setIsLoading(false);
+      if (r.ok) {
+        r.json().then((user) => onLogin(user));
+      } else {
+        r.json().then((err) => setErrors(err.errors));
+      }
+      });
+  }
 
-//fetch function (post request)
-function handleSubmit(e) {
-      e.preventDefault();
-      setIsLoading(true);
-      fetch("/login", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ username, password }),
-      })
-        .then((r) => {
-          setIsLoading(false);
-        if (r.ok) {
-          r.json().then((user) => onLogin(user));
-        } else {
-          r.json().then((err) => setErrors(err.errors));
-        }
-        });
-    }
-  
   return (
-      <form onSubmit={handleSubmit}>
-      <FormField>
-            <label htmlFor='username'>Username</label>
-            <Input 
-            type="text"
-            id="username"
-            autoComplete="off"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            />
+    <form onSubmit={handleSubmit}>
+    <FormField>
+        <Label htmlFor="username">Username</Label>
+        <Input
+          type="text"
+          id="username"
+          autoComplete="off"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
       </FormField>
-
       <FormField>
-            <Label htmlFor="password">Password</Label>
-            <Input
-            type="password"
-            id="password"
-            autoComplete="current-password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            />
+        <Label htmlFor="password">Password</Label>
+        <Input
+          type="password"
+          id="password"
+          autoComplete="current-password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
       </FormField>
-
-      <Button variant="fill" color="primary" type="submit">
-            {isLoading ? "Loading..." : "Login"}
-            </Button>
-            
+      
+        <Button variant="fill" color="primary" type="submit">
+          {isLoading ? "Loading..." : "Login"}
+        </Button>
+      
       <FormField>
         {errors.map((err) => (
           <Error key={err}>{err}</Error>
         ))}
-
       </FormField>
-</form>
-
-
-   
-  )
+    </form>
+  );
 }
