7a57531616624e183edfc5412023481c73a653b3
kanyi-a <angela.kanyi@student.moringaschool.com>
2022-10-10 00:41:09 +0300

    milestone:new column added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_09_185817) do
+ActiveRecord::Schema.define(version: 2022_10_09_192859) do
 
   create_table "admins", force: :cascade do |t|
     t.string "name"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 2022_10_09_185817) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password"
   end
 
 end
