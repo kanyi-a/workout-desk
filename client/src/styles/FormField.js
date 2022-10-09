b9582e77f8a904508f1775491a608e34b015ff99
kanyi-a <angela.kanyi@student.moringaschool.com>
2022-10-09 23:27:16 +0300

    milestone:importing a Formfield wrapper from styled components

---
+++ client/src/styles/FormField.js
@@ -0,0 +1,12 @@
+//import React from "react";
+
+import styled from "styled-components";
+
+const FormField = styled.div`
+&:not(:last-child) {
+  margin-bottom: 25px;
+  opacity: 0.6;
+}
+`;
+
+export default FormField;
