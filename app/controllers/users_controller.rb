ffb3afc274769dadf2144340b7b8118f7a645160
kanyi-a <angela.kanyi@student.moringaschool.com>
2022-10-10 00:57:31 +0300

    milestone:added create method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,9 @@ class UsersController < ApplicationController
             users = User.all
             render json: users, status: :ok
       end
+      def create
+            user = User.create!(user_params)
+            session[:user_id] = user.id
+            render json: user, status: :created
+      end
 end
