006f0d62a0bbafa2b256777a1aa60c1300659cdd
kanyi-a <angela.kanyi@student.moringaschool.com>
2022-10-10 13:24:35 +0300

    milestone:added attributes

--- app/controllers/stretch_poses_controller.rb
+++ app/controllers/stretch_poses_controller.rb
@@ -1,5 +1,27 @@
 class StretchPosesController < ApplicationController
-def index
-      stretchposes = StretchPose.all
-      render json: stretchposes, status: :ok
-end
+      rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response     
+      def index
+            stretchposes = StretchPose.all
+            render json: stretchposes, status: :ok
+      end
+      def show
+            stretchpose = find_stretch_poses
+            render json: stretchpose, status: :ok
+      end
+      def update
+            stretchpose = find_stretch_poses
+            stretchpose.update!(stretch_params)
+            render json: stretchpose, status: :ok
+      end 
+
+      private
+      def find_stretch_poses
+            StretchPose.find(params[:id])
+      end 
+      def stretch_params
+            params_permit(:is_favoured)
+      end 
+      def render_not_found_response
+            render json: {error: "Stretch Pose Currently Not Available "}, status: :not_found 
+      end 
+end 
