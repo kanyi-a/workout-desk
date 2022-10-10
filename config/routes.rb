c63728f18e7119d3b2b3f5d9088bd38c5f426b4e
kanyi-a <angela.kanyi@student.moringaschool.com>
2022-10-10 21:33:41 +0300

    milestone:added all CRUD request for  comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
+  resources :comments 
   resources :users, only: [:index]
   resources :stretch_poses, only: [:index, :show, :update]
   post "/signup", to: "users#create"
