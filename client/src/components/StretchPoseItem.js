ca1a2ca734f62ea5e981bc9f3e9e7cef335be5ff
kanyi-a <angela.kanyi@student.moringaschool.com>
2022-10-11 09:14:23 +0300

    milestone:adding the file to hold a specific stretch tutorial

---
+++ client/src/components/StretchPoseItem.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default function StretchPoseItem({ stretchPose, handleUpdateItem, handleFindVideoData }) {
+
+  const { id, title, image, category, description, is_favoured: is_favoured } = stretchPose;
+
+
+
+  function handleClick(e) {
+    e.preventDefault();
+
+    fetch(`http://localhost:4000/stretch_poses/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ is_favoured: !is_favoured }),
+    })
+      .then((r) => r.json())
+      .then((updatedItem) => handleUpdateItem(updatedItem))
+  }
+  
+  function handleItemClick(){
+      handleFindVideoData(stretchPose)
+ }
+
+  const summary = description.substring(0, 50)
+  
+
+  return (
+    <div className="stretch-pose-item">
+      <div>
+        <h2 className="stretch-card-title">{title}</h2>
+        <img src={image} className="stretchimage" alt="stretch pose"/>
+        <h4 className="stretch-card-category">{category}</h4>
+        <p className="stretch-card-description">{summary}...</p>
+      </div>
+      <Link exact to="/video" onClick={handleItemClick} ><i class="gg-play-button-o"></i></Link>
+    </div>
+  );
+}
