6c2a90b8c4d1ef0cb976fc5f5d0bff3a84c2253f
kanyi-a <angela.kanyi@student.moringaschool.com>
2022-10-11 22:34:47 +0300

    milestone:handles the DELETE request for the comments

---
+++ client/src/components/CommentBubble.js
@@ -0,0 +1,48 @@
+import React, {useState} from 'react';
+import EditComment from './EditComment';
+import { Link } from 'react-router-dom';
+
+export default function CommentBubble({ id, comment, handleDeleteComment, handleUpdateComment, handleShowComment }) {
+
+  const [isEditing, setIsEditing] = useState(false);
+  const { description, user } = comment
+  
+  function handleDelete(){
+    fetch(`/comments/${id}`, { 
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    handleDeleteComment(id)
+  }
+
+  return (
+     <div className="bubble">
+       {isEditing ? (
+        <EditComment
+          id={id}
+          comment={comment}
+          setIsEditing={setIsEditing}
+          handleUpdateComment={handleUpdateComment}
+          handleShowComment={handleShowComment}
+        />
+       ) : (
+         <div className="comment-container">
+           {/* <Link className="full-comment-button" to={`/comments/${id}`}>Full Comment</Link>  */}
+           <button className="delete-button" onClick={handleDelete}>X</button>
+           <button className="edit-btn" onClick={() => setIsEditing((isEditing) => !isEditing)}>
+              <span role="img" aria-label="edit">
+                ✏️
+              </span>
+            </button>
+            <br/>
+           <h3 className="comment">{description} 
+           <p className="username">- {user.username}</p>
+           <Link className="full-comment-button" to={`/comments/${id}`}>Full Comment</Link></h3>
+           </div>
+       )
+      }
+      </div>
+  )
+}
